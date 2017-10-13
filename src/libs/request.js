0f285d0f95ef808bf7b054474c172f7e437a5179
voson <i@voson.me>
2017-10-13 23:57:47 +0800

    错误、警告提醒的时长

--- src/libs/request.js
+++ src/libs/request.js
@@ -9,9 +9,15 @@ request.responseCheck = ({result, status}, success) => {
     if (status === 'success') {
         success()
     } else if (status === 'warning') {
-        iView.Message.warning('注意：'+result);
+        iView.Message.warning({
+            content: '注意：'+result,
+                duration: 5
+        });
     } else {
-        iView.Message.error('错误：'+result)
+        iView.Message.error({
+            content: '错误：'+result,
+                duration: 5
+        })
     }
 };
 
